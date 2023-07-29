1e68ca8697fad139dcf3f7568c1a2e0ee30d73e8
권태헌 <honth@naver.com>
2023-07-29 02:11:17 +0000

    init blog

---
+++ install_script.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -x
+
+patterns=('*.html' '*.js' 'favicon*' '*.css' '*.png' 'img' 'ads.txt')
+
+wp_path=/home/ubuntu/wordpress/wordpress
+git_path=/home/ubuntu/wordpress/kceleb_client
+cd $git_path
+for pat in ${patterns[@]}; do 
+	sudo rm -rf $wp_path/$pat
+	sudo cp -r $pat $wp_path/
+	sudo chown -R 82:82 $wp_path/$pat
+done
