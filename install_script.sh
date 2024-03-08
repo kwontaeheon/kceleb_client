bbbf59e6d1401eeaa6b0c04a632e6608df2d40b6
권태헌[Terry][AI Data Dev] <terry.kwon@navercorp.com>
2024-03-08 13:36:54 +0900

    client

--- install_script.sh
+++ install_script.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-patterns=('*.html' '*.js' 'favicon*' '*.css' '*.png' 'img' '*.txt' 'animaltest' 'puppy' '*.json' '*.webmanifest' '*.xml')
+patterns=('*.html' '*.js' 'favicon*' '*.css' '*.png' 'img' '*.txt' 'animaltest' 'puppy' '*.json' '*.webmanifest' '*.xml' 'client')
 
 wp_path=/home/ubuntu/wordpress/wordpress
 git_path=/home/ubuntu/wordpress/kceleb_client
