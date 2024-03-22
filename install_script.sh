1bb6ec428eebaf75e0e151f4ff6af1496a5090d2
kwontaeheon <kwontaeheon@gmail.com>
2024-03-22 21:41:58 +0900

    lang

--- install_script.sh
+++ install_script.sh
@@ -2,7 +2,7 @@
 
 set -x
 
-patterns=('*.html' '*.js' 'favicon*' '*.css' '*.png' 'img' '*.txt' 'animaltest' 'puppy' '*.json' '*.webmanifest' '*.xml' 'client', 'v_*')
+patterns=('*.html' '*.js' 'favicon*' '*.css' '*.png' 'img' '*.txt' 'animaltest' 'puppy' '*.json' '*.webmanifest' '*.xml' 'client' 'v_*' 'en' 'es' 'id' 'ja' 'ko' 'vi' 'zh')
 
 wp_path=/home/ubuntu/wordpress/wordpress
 git_path=/home/ubuntu/wordpress/kceleb_client
