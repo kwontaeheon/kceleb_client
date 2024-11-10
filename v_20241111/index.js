3e3f6de640e0929ea02e5fe68265d0705aee8573
kwontaeheon <kwontaeheon@gmail.com>
2024-11-11 01:13:10 +0900

    버전변경

--- v_20241111/index.js
+++ v_20241111/index.js
@@ -15,7 +15,7 @@ var faceData;
 
 
 const lang = $("#lang option:selected").val();
-const version = "/v_20241109";
+const version = "/v_20241111";
 var faceNames = {};
 var faceNamesKo = {};
 const jsonContainer = document.getElementById('json-container');
