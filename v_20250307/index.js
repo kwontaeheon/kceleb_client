67e4004e3cba7c232bea09844a72a0c021f433d9
Terry <honth@naver.com>
2025-03-07 21:11:47 +0900

    script version

--- v_20250307/index.js
+++ v_20250307/index.js
@@ -15,7 +15,7 @@ var faceData;
 
 
 const lang = $("#lang option:selected").val();
-const version = "/v_20250228";
+const version = "/v_20250307";
 var faceNames = {};
 var faceNamesKo = {};
 const jsonContainer = document.getElementById('json-container');
