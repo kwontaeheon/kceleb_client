58becb208631baccb378572755a586a9c64c589d
권태헌[Terry][AI Data Dev] <terry.kwon@navercorp.com>
2025-06-07 23:08:19 +0900

    v_20250607

--- v_20250607/index.js
+++ v_20250607/index.js
@@ -14,7 +14,7 @@ var faceData;
 
 
 const lang = $("#lang option:selected").val();
-const version = "/v_20250307";
+const version = "/v_20250607";
 var faceNames = {};
 var faceNamesKo = {};
 const jsonContainer = document.getElementById('json-container');
